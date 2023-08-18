0e6a4aeb75bc48e41c69af7c7aa41c04a89771e1
justkoi42 <justkoi42@gmail.com>
2023-08-18 21:35:05 +0530

    Write addValue function to get a pokemon form an input

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('ayaya');
+//Use type, region, generation, color, evo_chain. If result > 1 use starting skill. 
+
+const pokemonNameInput = document.querySelector('#pokemon-name-input');
+const btn = document.querySelector('.btn');
+let pokemon;
+
+const getValue = function(event) {
+	event.preventDefault();
+	pokemon = pokemonNameInput.value.toLowerCase();
+	console.log(pokemon);
+}
+
+btn.addEventListener('click', getValue);
+
+let img = document.querySelector('.pok-pic');
+
+async function getPokemon() {
+	const response = await fetch('https://pokeapi.co/api/v2/pokemon/squirtle');
+	const pokemonData = await response.json();
+		img.src = pokemonData.sprites.front_default;
+}
+
+getPokemon();
