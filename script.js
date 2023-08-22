b3c4715b146183abb0af1dfaa054a8e863366fcb
justkoi42 <justkoi42@gmail.com>
2023-08-22 23:31:35 +0530

    Request data from API

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-//Use type, region, generation, color, evo_chain. If result > 1 use starting skill. 
-
+//Type, Region, Color, Ability, Number of Evolution(How many evolutions pokemon has), Order of Evolution(Order in evolution chain)
+//Rename the app into Pokeguesser
 const pokemonNameInput = document.querySelector('#pokemon-name-input');
 const btn = document.querySelector('.btn');
-let pokemon;
+const pokemonBox = document.querySelector('svg');
 
-const getValue = function(event) {
+const getPokemonData = async function(event, pokemon) {
 	event.preventDefault();
+
 	pokemon = pokemonNameInput.value.toLowerCase();
-	console.log(pokemon);
+	const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+	const info = await response.json();
+	setPokemon(info.sprites.front_default);
 }
 
-btn.addEventListener('click', getValue);
+btn.addEventListener('click', getPokemonData);
 
-let img = document.querySelector('.pok-pic');
-
-async function getPokemon() {
-	const response = await fetch('https://pokeapi.co/api/v2/pokemon/squirtle');
-	const pokemonData = await response.json();
-		img.src = pokemonData.sprites.front_default;
+const setPokemon = function(pokemon) {
+	let pokemonPicture = document.createElement('img');
+	pokemonPicture.setAttribute('src', pokemon);
+	pokemonBox.replaceWith(pokemonPicture);
 }
 
-getPokemon();
+const setData = function(...args) {
+	console.log(args);
+}
